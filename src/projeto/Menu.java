616a084582e49bc105bc741f366a037c07abf170
EduardoAguiar15 <aguiareduardo678@outlook.com>
2023-09-07 14:09:23 -0300

    Trabalho final da disciplina de Programação orientada a objetos

--- src/projeto/Menu.java
+++ src/projeto/Menu.java
@@ -4,13 +4,12 @@ import java.util.Scanner;
 
 public class Menu {
 	
-	public int opcao;
 	
-	public static String Opcao() {
+	public static void Opcao() {
 		Scanner entrada = new Scanner(System.in);
 		System.out.println("Informe a opção desejada: \n1 - Adicionar estudante "
 + "\n2 - Editar estudante \n3 - Remover estudante \n4 - Listar estudantes \n5 - Sair");
-		 String opcao = entrada.next();
+		 int opcao = entrada.nextInt();
 		 
 		 switch(opcao) {
 		 case 1:
@@ -18,9 +17,10 @@ public class Menu {
 		 case 3:
 		 case 4:
 		 case 5:
+		 default:
 			 
 		 }
 	
+	 entrada.close();
 	}
-	
 }
